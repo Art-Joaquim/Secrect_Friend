0b33bdbed729d65a22d0ba5c0e82d3c919b931a7
Arthur Felipe <arthur.joaquim754@gmail.com>
2025-06-13 19:58:52 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,66 @@
+let amigos = [];
+
+function adicionar(){
+    let amigo = document.getElementById("nome-amigo").value.trim();
+    let lista = document.getElementById("lista-amigos");
+
+    if(amigo == ""){
+        alert("Informe o nome de um amigo.");
+        return;
+    }
+
+    if(amigos.includes(amigo)){
+        alert("Este amigo já está na lista.");
+        document.getElementById("nome-amigo").value = "";
+        return;
+    }
+
+    amigos.push(amigo);
+    amigos.sort();
+    document.getElementById("nome-amigo").value = "";
+    lista.textContent = amigos.join(", ");
+    document.getElementById("nome-amigo").focus();
+}
+
+function sortear(){
+    embaralha(amigos);
+    document.getElementById("lista-sorteio").textContent = "";
+    let sorteio = document.getElementById("lista-sorteio");
+    
+    for(let i=0; i < amigos.length; i++){
+        if(i == amigos.length-1){
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + "->" + amigos[0] + "<br>";
+        }else{
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + "->" + amigos[i+1] + "<br>";
+        }
+    }
+
+}
+
+function reiniciar(){
+    amigos = [];
+    document.getElementById("nome-amigo").value = "";
+    document.getElementById("lista-amigos").textContent = "";
+    document.getElementById("lista-sorteio").textContent = "";
+
+}
+
+function embaralha(lista){
+
+    for (let indice = lista.length; indice; indice--) {
+
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+
+        // atribuição via destructuring
+        [lista[indice - 1], lista[indiceAleatorio]] = 
+            [lista[indiceAleatorio], lista[indice - 1]];
+    }
+}
+
+window.onload = function() {
+    document.getElementById("nome-amigo").addEventListener("keyup", function(event) {
+        if (event.key === "Enter") {
+            adicionar();
+        }
+    });
+}
